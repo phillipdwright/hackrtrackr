e3616ad5e760c320675408ee9565a99ba75cafa1
Phil <wright.phillip@gmail.com>
2016-07-05 20:11:36 +0000

    Add settings file

---
+++ hackrtrackr/settings.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE_NAME = 'hackrtrackr.db'
+# DATABASE_NAME = 'testDB13.db'
+#DATABASE_NAME = 'testDB14.db'
+#DATABASE_NAME = 'test_autoupdate.db'
+DEBUG = True
